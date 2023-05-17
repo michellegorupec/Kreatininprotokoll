624ff27de1ae3f100737a280f3dc47c8f5b19267
michellegorupec <131359228+michellegorupec@users.noreply.github.com>
2023-05-17 10:23:55 +0200

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 11:07:53 2023
+
+@author: Michelle & Katja
+"""
+
+import streamlit as st
+import pandas as pd
+from jsonbin import load_key, save_key
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+
+st.set_page_config(page_title="Kreatininprotokoll und GFR")
+
+# -------- load secrets for jsonbin.io --------
+jsonbin_secrets = st.secrets["jsonbin"]
+api_key = jsonbin_secrets["api_key"]
+bin_id = jsonbin_secrets["bin_id"]
+
+# -------- user login --------
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
+
+fullname, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
+
+data = load_key(api_key, bin_id, username)
+
+# Startseite mit den wichtigsten Informationen und dem Zugriff zu den weiteren Seiten    
+st.title("Das wichtigste zu Kreatinin und GFR")
+
+st.markdown(
+        """
+        ## Kreatinin:
+        Kreatinin ist ein Abbauprodukt des Kreatinphosphats, das in der Muskulatur gebildet wird. 
+        Es spielt eine Hauptrolle im Enegiestoffwechsel der Skelettmuskulatur, vor allem bei kurzzeitiger Muskelarbeit.  
+        Die Konzentration von Kreatinin um Blut ist ein wichtiger Indikator für die Nierenfunktion. 
+        Da die Nieren das Kreatinin aus dem Blut filtrieren und über den Urin ausscheiden.
+        
+        ### Referenzbereich:
+        Je nach Alter und Geschlecht variiert die angemessene Kreatinin-Konzentration im Blut. 
+        
+        Frauen: 0.58 - 1.20 mg/dL
+        
+        Männer: 0.77 - 1.47 mg/dL
+    
+        Ihre Werte sollten Sie am Besten noch mit Ihrem Hausarzt besprechen.
+    
+        ## Glomeruläre Filtrationsrate:
+        Die glomeruläre Filtrationsrate (GFR) ist der wichtigste Parameter zur Abschätzung der Nierenfunktion, 
+        da sie anzeigt, wie gut die Nieren das Blut filtern und Abfallprodukte aus dem Körper entfernen.
+        Zu hohe Kreatininwerte können auf eine Nierenschwäche, Verlertzung der Muskulatur, Muskeldystrophie
+        oder eine Entzündung der Haut und Muskulatur hindeuten. 
+        Zu niedrige GFR-Werte weisen auf eine Nierenschwäche hin bis zu einer Niereninsuffizienz.
+        Die Therapie einer Niereninsuffizienz erfolgt mit einer Dialyse (Reinigung des Blutes) oder einer Spenderniere.
+        
+        ### Referenzbereich:
+        Das Alter, Hautfarbe und Geschlecht können eine Rolle spielen, da sie alle Einfluss auf die Muskelmasse haben.
+        Die Muskelmasse nimmt im Allgemeinen mit dem Alter ab, deswegen kann die GFR bei älteren Menschen niedriger sein.
+        Männer haben mehr Muskelmasse als Frauen, was wiederum bedeutet, dass die GFR im Allgemeinen bei Männern höher ist.
+        Menschen mit dunkler Haut haben höhere Muskelmasse als Menschen mit heller Haut, was wiederum dazu führt dass sie höhere Werte haben.
+        
+        Der normale GFR liegt bei ca. 90 - 120 mL/min, jedoch nimmt dieser ab dem 40. Lebensjahr um etwa 1 mL/min pro Jahr altersbedingt ab.
+    
+        👈 Über die Sidebar gelangen Sie zum Kreatinin-Protokoll und zum GFR-Rechner!
+    """
+    )
---
+++ pages/1_Kreatininwerte.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+from jsonbin import load_key, save_key
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+
+DATA_FILE = "data.json"
+
+# Funktion zum Laden der Standards aus einer JSON-Datei
+# def load_key(api_key, bin_id, key, empty_value=[]):
+ #   if os.path.isfile(DATA_FILE):
+  #      with open(DATA_FILE, "r", encoding="utf-8") as file:
+   #         data = json.load(file)
+   # else:
+    #    data = []
+    #return data
+    
+# -------- load secrets for jsonbin.io --------
+jsonbin_secrets = st.secrets["jsonbin"]
+api_key = jsonbin_secrets["api_key"]
+bin_id = jsonbin_secrets["bin_id"]
+# -------- user login --------
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
+
+fullname, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
+data = load_key(api_key, bin_id, username)
+
+# Funktion zum Speichern der Standards in einer JSON-Datei
+def save_key(api_key, bin_id, key, data):
+    with open(DATA_FILE, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+# Seite darstellen
+st.set_page_config(page_title="Kreatinin Bestimmung", page_icon="📊")
+st.markdown("# 📊 Kreatinin Bestimmung")
+st.write(
+    """
+    Auf dieser Seite können die Daten erfasst werden.
+    """
+)
+
+# Daten aus Datei laden
+data = load_key()
+
+# Seite aufbauen
+creatinine = st.number_input("Kreatinin (in mg/dL)", min_value=0.1, max_value=20.0, step=0.1, key="creatinine")
+date = st.date_input("Datum", key="date")
+creatinine_data = {"date": str(date), "value": creatinine}
+save_button = st.button("Speichern")
+
+# Sidebar
+delete_all_button = st.sidebar.button("Alles löschen")
+
+# Leere Zeile einfügen (Abstand zwischen Button und Graphen)
+st.write("")
+
+# Einen neuen Eintrag generieren, anhängen und in Datei schreiben
+if save_button == True:
+    new_entry = {
+        "Datum": str(date), 
+        "Kreatinin in mg/dL": creatinine
+        }
+    data.append(new_entry)
+    save_key(data)
+
+# Alle Datensätze löschen und in Datei speichern 
+if delete_all_button == True:
+    data = []
+    save_key(data)
+    
+# Datenframe erzeugen und als Graphen anzeigen. X-Achse ist das Datum.
+df = pd.DataFrame(data)
+chart = st.line_chart(df, 
+                      x = "Datum", 
+                      use_container_width = True)
+
+# Alle Daten als Liste darstellen
+st.dataframe(df, use_container_width=True)
+    
